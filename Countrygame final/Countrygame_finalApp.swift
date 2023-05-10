a5e51ee2c2fe9661afcf89bac6058af88f247196
Jakub <Wilk>
2023-05-10 09:42:20 -0500

    Initial Commit

---
+++ Countrygame final/Countrygame_finalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Countrygame_finalApp.swift
+//  Countrygame final
+//
+//  Created by Jakub Wilk on 5/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Countrygame_finalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
