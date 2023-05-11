01188c456183be28a2c0b5e061b2031c5ce9f66d
Jakub <Wilk>
2023-05-11 12:09:05 -0500

    fix

---
+++ Countrygame final/components/CustomButton.swift
@@ -0,0 +1,30 @@
+//
+//  CustomButton.swift
+//  Countrygame final
+//
+//  Created by Jakub Wilk on 5/10/23.
+//
+
+import SwiftUI
+
+struct CustomButton: View {
+    var text: String
+    var background: Color = .yellow
+    var body: some View {
+        Text(text)
+            .foregroundColor(.cyan)
+            .padding()
+            .padding(.horizontal)
+            .background(background)
+            .cornerRadius(25)
+            .shadow(radius: 10)
+    }
+}
+
+struct CustomButton_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomButton(text:"Next")
+    }
+}
+
+
